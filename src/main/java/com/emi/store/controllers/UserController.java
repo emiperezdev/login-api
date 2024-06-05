2329d3c337d2b534d83bd094d9cb4f071767af1d
emiperezdev <emiperez.dev@gmail.com>
2024-06-05 14:19:01 -0600

    Change url api.

--- src/main/java/com/emi/store/controllers/UserController.java
+++ src/main/java/com/emi/store/controllers/UserController.java
@@ -22,7 +22,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 
 @RestController
-@RequestMapping("/todo-app/users")
+@RequestMapping("/api/users")
 @RequiredArgsConstructor
 public class UserController {
 
